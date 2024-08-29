24fbda1d5b1657fdcad90817f8a55e006c04a725
lin-cadez <lin.cadez@gmail.com>
2024-08-30 01:37:44 +0200

    chore: Update vite.config.js to include vite-plugin-node

--- NationsV2/vite.config.js
+++ NationsV2/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import node from 'vite-plugin-node';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), node()],
+});
