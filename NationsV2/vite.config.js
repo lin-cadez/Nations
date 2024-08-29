0b2cd11d06c758b84f64ad8e177801579ce31088
lin-cadez <lin.cadez@gmail.com>
2024-08-30 01:40:00 +0200

    chore: Update vite.config.js to remove vite-plugin-node

--- NationsV2/vite.config.js
+++ NationsV2/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import node from 'vite-plugin-node';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), node()],
-});
+  plugins: [react()],
+})
